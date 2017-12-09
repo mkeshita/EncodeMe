cc4b0f97d0b3464d0607ee28400826522d9721bf
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2017-12-10 03:12:13 +0800

    Initialized Server Project

---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace NORSU.EncodeMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
