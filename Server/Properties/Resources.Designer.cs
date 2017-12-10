2e343e3e5f5460722e5516b4e0e2171415dddc8b
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2017-12-10 21:23:34 +0800

    Added reference to MaterialDesignThemes

--- Server/Properties/Resources.Designer.cs
+++ Server/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NORSU.EncodeMe.Properties {
+namespace Server.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace NORSU.EncodeMe.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NORSU.EncodeMe.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Server.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
