d5c3ff537126294cbbfb9633d6c23050eb8dd927
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2017-12-10 21:34:48 +0800

    added DelegateCommand

---
+++ Server/Commands.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using NORSU.EncodeMe.Models;
+
+namespace Server
+{
+    static class Commands
+    {
+        private static ICommand _SaveCommand;
+
+        public static ICommand Save => _SaveCommand ??
+                                       (_SaveCommand =
+                                           new DelegateCommand<ModelBase>(m => m.Save(), m => m.CanSave()));
+
+        private static ICommand _deleteCommand;
+
+        public static ICommand Delete => _deleteCommand ??
+                                         (_deleteCommand =
+                                             new DelegateCommand<ModelBase>(m => m.Delete(),
+                                                 m => m.CanDelete()));
+    }
+}
---
+++ Server/DelegateCommand.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Server
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private Action<T> _execute;
+        private Predicate<T> _canExecute;
+
+        private event EventHandler CanExecuteChangedInternal;
+
+        public DelegateCommand(Action<T> execute)
+            : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException(nameof(canExecute));
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //try
+            //{
+            if (parameter != null && typeof(T) != parameter.GetType()) return false;
+            return _canExecute != null && _canExecute((T)parameter);
+            //}
+            //catch (Exception)
+            //{
+            //  return false;
+            //}
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            var handler = this.CanExecuteChangedInternal;
+            handler?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Destroy()
+        {
+            _canExecute = _ => false;
+            _execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(T parameter)
+        {
+            return true;
+        }
+    }
+
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        public DelegateCommand(Action<object> execute) : base(execute)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute) : base(execute, canExecute)
+        {
+        }
+    }
+}
