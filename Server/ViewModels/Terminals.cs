302904f66e82c00a3afea6058242d1e7c78eb516
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-02-23 20:52:36 +0800

    print log

--- Server/ViewModels/Terminals.cs
+++ Server/ViewModels/Terminals.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using MaterialDesignThemes.Wpf;
 using NORSU.EncodeMe.Models;
 using NORSU.EncodeMe.Network;
+using Xceed.Words.NET;
+using VerticalAlignment = System.Windows.VerticalAlignment;
 
 namespace NORSU.EncodeMe.ViewModels
 {
@@ -93,6 +98,130 @@ namespace NORSU.EncodeMe.ViewModels
                 return _items;
             }
         }
+        
+        internal static void Print(string path)
+        {
+            var info = new ProcessStartInfo(path);
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.UseShellExecute = true;
+            info.Verb = "PrintTo";
+            try
+            {
+                Process.Start(info);
+            } catch(Exception e)
+            {
+                //
+            }
+        }
+
+        private bool _IsPrinting;
+
+        public bool IsPrinting
+        {
+            get => _IsPrinting;
+            set
+            {
+                if(value == _IsPrinting)
+                    return;
+                _IsPrinting = value;
+                OnPropertyChanged(nameof(IsPrinting));
+            }
+        }
+        
+        private ICommand _printSuggestionsCommand;
+
+        public ICommand PrintLogCommand =>
+            _printSuggestionsCommand ?? (_printSuggestionsCommand = new DelegateCommand(
+            d =>
+            {
+                IsPrinting = true;
+                Task.Factory.StartNew(() =>
+                {
+
+                
+                if(!(Items.CurrentItem is Client client)) return;
+
+                if(!Directory.Exists("Temp"))
+                    Directory.CreateDirectory("Temp");
+
+                var temp = Path.Combine("Temp", $"TerminalLog [{DateTime.Now:yy-MMM-dd}].docx");
+                var template = $@"Templates\TerminalLog.docx";
+
+                var fontSize = 0d;
+                var borders = new List<double>();
+                using(var doc = File.Exists(template) ? DocX.Load(template) : DocX.Create(temp))
+                {
+                    var tbl = doc.Tables.FirstOrDefault();
+                    if(tbl == null)
+                    {
+                        tbl = doc.AddTable(1, 3);
+
+                        var r = tbl.Rows[0];
+                        r.Cells[0].Paragraphs.First().Append("TYPE").Alignment = Alignment.center;
+                        r.Cells[1].Paragraphs.First().Append("TIME").Alignment = Alignment.center;
+                        r.Cells[2].Paragraphs.First().Append("EVENT").Alignment = Alignment.center;
+                    }
+
+
+                    for(int i = 0; i < tbl.ColumnCount; i++)
+                    {
+                        borders.Add(tbl.Rows[0].Cells[i].Width);
+                    }
+                    
+                    foreach(Models.TerminalLog item in Logs)
+                    {
+                        var r = tbl.InsertRow();
+                    
+                        var p = r.Cells[0].Paragraphs.First().Append(item.Type.ToString());
+                        p.LineSpacingAfter = 0;
+                        p.Alignment = Alignment.center;
+                        
+                        p = r.Cells[1].Paragraphs.First().Append(item.Time.ToString("MM-dd-yyyy hh:mm tt"));
+                        p.LineSpacingAfter = 0;
+                        p.Alignment = Alignment.center;
+
+                        p = r.Cells[2].Paragraphs.First().Append(item.Message);
+                        p.LineSpacingAfter = 0;
+                        p.Alignment = Alignment.left;
+                        
+                        for(var i = 0; i < borders.Count; i++)
+                        {
+                            r.Cells[i].Width = borders[i];
+                            //r.Cells[i].Paragraphs.First().FontSize(10);
+                        }
+                    }
+                    
+                    if(doc.Tables.Count==0)
+                        doc.InsertTable(tbl);
+
+                    doc.ReplaceText("[NAME]", client.Hostname);
+                    doc.ReplaceText("[IP]", client.IP);
+
+                    var border = new Xceed.Words.NET.Border(BorderStyle.Tcbs_single, BorderSize.one, 0,
+                        System.Drawing.Color.Black);
+                    tbl.SetBorder(TableBorderType.Bottom, border);
+                    tbl.SetBorder(TableBorderType.Left, border);
+                    tbl.SetBorder(TableBorderType.Right, border);
+                    tbl.SetBorder(TableBorderType.Top, border);
+                    tbl.SetBorder(TableBorderType.InsideV, border);
+                    tbl.SetBorder(TableBorderType.InsideH, border);
+
+                    try
+                    {
+                        File.Delete(temp);
+                    } catch(Exception e)
+                    {
+                        //
+                    }
+
+                    doc.SaveAs(temp);
+                }
+
+                Print(temp);
+                    IsPrinting = false;
+                });
+            }));
 
         private ListCollectionView _logs;
 
