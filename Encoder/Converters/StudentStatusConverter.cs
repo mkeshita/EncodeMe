48c2dd2653e0fe9f7a62120bf55ed4f8ca692c7e
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-02-25 07:02:25 +0800

    closes #48

---
+++ Encoder/Converters/StudentStatusConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NORSU.EncodeMe.Converters
+{
+    class StudentStatusConverter  :ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            var stat = (int) value;
+            if (stat == 0) return "OnGoing";
+            if (stat == 1) return "Returnee";
+            if (stat == 2) return "Shiftee";
+            if (stat == 3) return "Transferee";
+            
+            return "N/A";
+        }
+    }
+}
